60bf3e7304423de18865370fe22a9cb4f997a2c0
Jared <mumawjared@gmail.com>
2022-08-22 18:50:08 -0700

    comments

--- app/models/Art.rb
+++ app/models/Art.rb
@@ -1,5 +1,5 @@
 class Art < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
-
+    has_many :comments
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
     has_many :arts
-    has_many :comments
+    has_many :comments, through: :arts
 end
