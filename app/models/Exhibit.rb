bd8d84269d0ae4be6d55e7b06eec7ddc9298430f
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-08-26 12:36:38 -0500

    delete first exhibit

--- app/models/Exhibit.rb
+++ app/models/Exhibit.rb
@@ -5,4 +5,14 @@ class Exhibit < ActiveRecord::Base
     def grab_exhibit
         Art.where(exhibit_id: self.id)
     end
+
+    def self.random_exhibit
+        Exhibit.order('RANDOM()').first
+    end
+
+    def self.delete_first_exhibit
+        first_exhibit = Exhibit.first
+        first_exhibit.destroy
+        Art.where(exhibit_id: first_exhibit.id).destroy_all
+    end
 end
